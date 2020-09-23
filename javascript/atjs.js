c8ae9b0f331c13cc5acc17287c9bf036d2d13df5
adyhariri <mahmoudalhariri1994@gmail.com>
2020-09-23 12:25:18 +0300

    askingTrip function added to js code

--- javascript/atjs.js
+++ javascript/atjs.js
@@ -1,13 +1,17 @@
 alert ('hello and welcome to ask trip site ')
-var x = prompt ('if you want to ask trip now please inter 1 , if not please inter 0');
-if  (x ==1) {
-              var y = prompt ('What is the distance between your location and the city center ?');
-              if (y <=3)
-              {
-                 alert ('captin muhammed ahmad will git you by his mercedes car through 5 min please press on ask trip button to confirm');
-              }
-              else if (y<=6)
-                  ('captin omar walid will git you by his bmw car through 10 min please press on ask trip button to confirm');
-                  else alert ('we abologize there is no car close of your place');
-            }
-else alert ('please take your time and have a fun on our site');
+function askingTrip() {
+   var x = prompt ('if you want to ask trip now please inter 1 , if not please inter 0');
+   if  (x ==1) {
+                 var y = prompt ('What is the distance between your location and the city center in kilometers ?');
+                 if (y <=3)
+                 {
+                    alert ('captin muhammed ahmad will git you by his mercedes car through 5 min please press on ask trip button to confirm');
+                 }
+                 else if (y>3&&y<=6)
+                   alert  ('captin omar walid will git you by his bmw car through 10 min please press on ask trip button to confirm');
+                     else alert ('we abologize there is no car close of your place');
+               }
+   else alert ('please take your time and have a fun on our site');
+   
+}
+askingTrip();
